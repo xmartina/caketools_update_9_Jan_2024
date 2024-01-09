d21c93f60d9b3f97247ac501601d53d05be8cf4d
xmartina <martinsprince126@gmail.com>
2024-01-09 10:38:57 +0100

    i

--- includes/auth.php
+++ includes/auth.php
@@ -51,15 +51,27 @@ if (isset($_POST['register'])) {
         $row = $result->fetch_assoc();
         $get_user_id = $row['id'];
 
-        $add_wallet_data = "INSERT INTO wallet ('wallet_ref_id', 'wallet_owner_id', 'wallet_key', 'wallet_status') VALUES ($w_ref_id, $get_user_id, 1, 3)";
+        $add_wallet = "INSERT INTO wallet (wallet_ref_id, wallet_owner_id, wallet_key, wallet_status) VALUES ($w_ref_id, $get_user_id, 1, 3)";
 
-        if (mysqli_query($conn, $add_wallet_data)) {
+        if (mysqli_query($conn, $add_wallet)) {
+
+            $get_user_data = "SELECT * FROM wallet WHERE wallet_owner_id = '$get_user_id'";
+            $result = $conn->query($get_user_data);
+            $row = $result->fetch_assoc();
+            $get_wallet_id = $row['wallet_id'];
+            $get_wallet_key = $row['wallet_key'];
+            if ($get_wallet_key == 1){
+                $get_wallet_key = 'meta_mask';
+            }
+
+            $add_wallet_data = "INSERT INTO wallet_data (d_wallet_parent_id, d_wallet_name, d_wallet_phase, d_wallet_owner_id, d_wallet_username) VALUES ($get_wallet_id, $get_wallet_key, 0, $get_user_id, 0)";
 
 
-            // Registration success
-            header('Location:' . siteUrl . 'auth/register?reg-success');
-            exit();
-
+            if (mysqli_query($conn, $add_wallet_data)) {
+                // Registration success
+                header('Location:' . siteUrl . 'auth/register?reg-success');
+                exit();
+            }
         }
     } else {
         // Registration failed
