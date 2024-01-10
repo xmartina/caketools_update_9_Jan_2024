d1206c85c92c9bf04b4be5803a73e5a768bc7869
xmartina <martinsprince126@gmail.com>
2024-01-10 10:26:53 +0100

    i

--- connect_wallet/parts/wallets/meta_mask.php
+++ connect_wallet/parts/wallets/meta_mask.php
@@ -8,7 +8,7 @@ include_once (rootDir.'includes/wallet/get/main.php');
             <img src="/assets/images/svg/icon-wallet-1.svg" alt="Image">
 
         </div>
-        <h6 class="title d-flex align-items-center"><a href="#"> Meta Mask </a> <?php if ($m_wallet_status == '2') { ?><div class="ml-3 p-2 bg-info text-white rounded" style="font-size: 12px; max-width: 120px;"> Pending Approval</div><?php }elseif ($m_wallet_status == '1') { ?><div class="ml-3 p-2 bg-light text-success rounded" style="font-size: 12px; max-width: 120px;"> Pending Approval</div> <?php } ?>
+        <h6 class="title d-flex align-items-center"><a href="#"> Meta Mask </a> <?php if ($m_wallet_status == '2') { ?><div class="ml-3 p-2 bg-info text-white rounded" style="font-size: 12px; max-width: 120px;"> Connected</div><?php }elseif ($m_wallet_status == '1') { ?><div class="ml-3 p-2 bg-light text-success rounded" style="font-size: 12px; max-width: 120px;"> Pending Approval</div> <?php } ?>
         </h6>
     </div>
 </div>
