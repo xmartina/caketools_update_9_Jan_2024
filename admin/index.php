ecbfc97837d7d6f3b24e24a18f53f781118c98df
xmartina <martinsprince126@gmail.com>
2024-01-13 21:36:08 +0100

    i

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 const adminRootDir = '/home/multistream6/domains/caketoolnftmarketplace.com/public_html/admin/';
 const adminUrl = 'https://caketoolnftmarketplace.com/admin/';
 $pageName = 'Dashboard';
