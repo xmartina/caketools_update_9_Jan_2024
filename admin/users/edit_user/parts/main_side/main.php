3d7e1c1ece36fe267c44838ea45d075150a4104f
xmartina <martinsprince126@gmail.com>
2024-01-13 21:05:04 +0100

    i

--- admin/users/edit_user/parts/main_side/main.php
+++ admin/users/edit_user/parts/main_side/main.php
@@ -1,7 +1,7 @@
 <div class="col-xl-8 col-lg-7 col-md-7 order-0 order-md-1">
     <!-- Project table -->
     <div class="card mb-4">
-        <h5 class="card-header">Connected Wallets</h5>
+        <h5 class="card-header">User Wallets</h5>
         <div class="card-body">
             <div class="row gx-3 gy-3">
                 <div class="col-lg-6">
@@ -100,3 +100,124 @@
         </div>
     </div>
     <!-- /Project table -->
+    <div class="row">
+        <!-- Meeting Schedule -->
+        <div class="col-xl-4 col-md-6">
+            <div class="card h-100">
+                <div class="card-header d-flex align-items-center justify-content-between">
+                    <h5 class="card-title m-0 me-2">Pending Wallet Connect</h5>
+                    <div class="dropdown">
+                        <button
+                            class="btn p-0"
+                            type="button"
+                            id="meetingSchedule"
+                            data-bs-toggle="dropdown"
+                            aria-haspopup="true"
+                            aria-expanded="false">
+                            <i class="mdi mdi-dots-vertical mdi-24px"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="meetingSchedule">
+                            <a class="dropdown-item" href="javascript:void(0);">Last 28 Days</a>
+                            <a class="dropdown-item" href="javascript:void(0);">Last Month</a>
+                            <a class="dropdown-item" href="javascript:void(0);">Last Year</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <ul class="p-0 m-0">
+                        <li class="d-flex mb-4 pb-1">
+                            <div class="avatar flex-shrink-0 me-3">
+                                <img src="<?=adminUrl?>assets/img/avatars/4.png" alt="avatar" class="rounded" />
+                            </div>
+                            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                <div class="me-2">
+                                    <h6 class="mb-0">Call with Woods</h6>
+                                    <small>
+                                        <i class="mdi mdi-calendar-blank-outline mdi-14px"></i>
+                                        <span>21 Jul | 08:20-10:30</span>
+                                    </small>
+                                </div>
+                                <div class="badge bg-label-primary rounded-pill">Business</div>
+                            </div>
+                        </li>
+                        <li class="d-flex mb-4 pb-1">
+                            <div class="avatar flex-shrink-0 me-3">
+                                <img src="<?=adminUrl?>assets/img/avatars/5.png" alt="avatar" class="rounded" />
+                            </div>
+                            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                <div class="me-2">
+                                    <h6 class="mb-0">Conference call</h6>
+                                    <small>
+                                        <i class="mdi mdi-calendar-blank-outline mdi-14px"></i>
+                                        <span>21 Jul | 08:20-10:30</span>
+                                    </small>
+                                </div>
+                                <div class="badge bg-label-warning rounded-pill">Dinner</div>
+                            </div>
+                        </li>
+                        <li class="d-flex mb-4 pb-1">
+                            <div class="avatar flex-shrink-0 me-3">
+                                <img src="<?=adminUrl?>assets/img/avatars/3.png" alt="avatar" class="rounded" />
+                            </div>
+                            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                <div class="me-2">
+                                    <h6 class="mb-0">Meeting with Mark</h6>
+                                    <small>
+                                        <i class="mdi mdi-calendar-blank-outline mdi-14px"></i>
+                                        <span>21 Jul | 08:20-10:30</span>
+                                    </small>
+                                </div>
+                                <div class="badge bg-label-secondary rounded-pill">Meetup</div>
+                            </div>
+                        </li>
+                        <li class="d-flex mb-4 pb-1">
+                            <div class="avatar flex-shrink-0 me-3">
+                                <img src="<?=adminUrl?>assets/img/avatars/14.png" alt="avatar" class="rounded" />
+                            </div>
+                            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                <div class="me-2">
+                                    <h6 class="mb-0">Meeting in Oakland</h6>
+                                    <small>
+                                        <i class="mdi mdi-calendar-blank-outline mdi-14px"></i>
+                                        <span>21 Jul | 08:20-10:30</span>
+                                    </small>
+                                </div>
+                                <div class="badge bg-label-danger rounded-pill">Dinner</div>
+                            </div>
+                        </li>
+                        <li class="d-flex mb-4 pb-1">
+                            <div class="avatar flex-shrink-0 me-3">
+                                <img src="<?=adminUrl?>assets/img/avatars/8.png" alt="avatar" class="rounded" />
+                            </div>
+                            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                <div class="me-2">
+                                    <h6 class="mb-0">Call with hilda</h6>
+                                    <small>
+                                        <i class="mdi mdi-calendar-blank-outline mdi-14px"></i>
+                                        <span>21 Jul | 08:20-10:30</span>
+                                    </small>
+                                </div>
+                                <div class="badge bg-label-success rounded-pill">Meditation</div>
+                            </div>
+                        </li>
+                        <li class="d-flex">
+                            <div class="avatar flex-shrink-0 me-3">
+                                <img src="<?=adminUrl?>assets/img/avatars/1.png" alt="avatar" class="rounded" />
+                            </div>
+                            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                <div class="me-2">
+                                    <h6 class="mb-0">Meeting with Carl</h6>
+                                    <small>
+                                        <i class="mdi mdi-calendar-blank-outline mdi-14px"></i>
+                                        <span>21 Jul | 08:20-10:30</span>
+                                    </small>
+                                </div>
+                                <div class="badge bg-label-primary rounded-pill">Business</div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <!--/ Meeting Schedule -->
+    </div>
