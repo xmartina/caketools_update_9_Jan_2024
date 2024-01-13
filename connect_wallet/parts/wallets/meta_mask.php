9dc8f499238eba20a956ba79bc4ea8f59ff8cc78
xmartina <martinsprince126@gmail.com>
2024-01-13 19:52:05 +0100

    i

--- connect_wallet/parts/wallets/meta_mask.php
+++ connect_wallet/parts/wallets/meta_mask.php
@@ -8,7 +8,7 @@ include_once (rootDir.'includes/wallet/get/main.php');
             <img src="/assets/images/svg/icon-wallet-1.svg" alt="Image">
 
         </div>
-        <h6 class="title d-flex align-items-center"><a href="#"> Meta Mask </a> <?php if ($m_wallet_status == '2') { ?><div class="ml-3 p-2 bg-info text-white rounded" style="font-size: 10px; max-width: 100px; min-width: 100px;"> Pending Approval</div><?php }elseif ($m_wallet_status == '1') { ?><div class="ml-3 p-2 text-success rounded" style="font-size: 12px; max-width: 120px; background-color: #fff!important;"> Connected</div> <?php } ?>
+        <h6 class="title d-flex align-items-center"><a href="#"><span class="name" style="max-width: 230px min-width: 230px">Meta Mask</span> </a> <?php if ($m_wallet_status == '2') { ?><div class="ml-3 p-2 bg-info text-white rounded" style="font-size: 10px; max-width: 100px; min-width: 100px;"> Pending Approval</div><?php }elseif ($m_wallet_status == '1') { ?><div class="ml-3 p-2 text-success rounded" style="font-size: 12px; max-width: 120px; background-color: #fff!important;"> Connected</div> <?php } ?>
         </h6>
     </div>
 </div>
