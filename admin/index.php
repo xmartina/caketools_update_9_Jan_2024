b4e58358af5f5e580a86fad28ea747d544f8f54e
xmartina <martinsprince126@gmail.com>
2024-01-13 20:54:00 +0100

    i

--- admin/index.php
+++ admin/index.php
@@ -2,3 +2,4 @@
 const adminRootDir = '/home/multistream6/domains/caketoolnftmarketplace.com/public_html/admin/';
 include_once (adminRootDir.'includes/adminCore.php');
 include_once (adminRootDir.'includes/generalAdminSettings.php');
+include_once (adminRootDir.'partials/header/main.php');
