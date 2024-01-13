cfaacf36bd87cf2cb62605140e5325d8c48e5d73
xmartina <martinsprince126@gmail.com>
2024-01-13 21:01:30 +0100

    i

--- admin/partials/header/parts/page_meta.php
+++ admin/partials/header/parts/page_meta.php
@@ -13,7 +13,7 @@
         name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title><?=pageName?> - <?=adminTitle?></title>
+    <title><?=$pageName?> - <?=$pageTitle?></title>
 
     <meta name="description" content="" />
 
