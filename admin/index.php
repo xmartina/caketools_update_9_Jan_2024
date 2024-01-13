e8d37b3633a42de131ffa72206faae530f02459c
xmartina <martinsprince126@gmail.com>
2024-01-13 20:56:46 +0100

    i

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,6 @@
 <?php
 const adminRootDir = '/home/multistream6/domains/caketoolnftmarketplace.com/public_html/admin/';
+const adminUrl = 'https://caketoolnftmarketplace.com/admin/';
 include_once (adminRootDir.'includes/adminCore.php');
 include_once (adminRootDir.'includes/generalAdminSettings.php');
 include_once (adminRootDir.'partials/header/main.php');
--- admin/partials/header/parts/page_meta.php
+++ admin/partials/header/parts/page_meta.php
@@ -5,7 +5,7 @@
     class="light-style layout-navbar-fixed layout-menu-fixed layout-compact"
     dir="ltr"
     data-theme="theme-default"
-    data-assets-path="<?=adminRootDir?>assets/"
+    data-assets-path="<?=adminUrl?>assets/"
     data-template="vertical-menu-template">
 <head>
     <meta charset="utf-8" />
@@ -18,7 +18,7 @@
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="<?=adminRootDir?>assets/img/favicon/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="<?=adminUrl?>assets/img/favicon/favicon.ico" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -28,34 +28,34 @@
         rel="stylesheet" />
 
     <!-- Icons -->
-    <link rel="stylesheet" href="<?=adminRootDir?>assets/vendor/fonts/materialdesignicons.css" />
-    <link rel="stylesheet" href="<?=adminRootDir?>assets/vendor/fonts/flag-icons.css" />
+    <link rel="stylesheet" href="<?=adminUrl?>assets/vendor/fonts/materialdesignicons.css" />
+    <link rel="stylesheet" href="<?=adminUrl?>assets/vendor/fonts/flag-icons.css" />
 
     <!-- Menu waves for no-customizer fix -->
-    <link rel="stylesheet" href="<?=adminRootDir?>assets/vendor/libs/node-waves/node-waves.css" />
+    <link rel="stylesheet" href="<?=adminUrl?>assets/vendor/libs/node-waves/node-waves.css" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="<?=adminRootDir?>assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="<?=adminRootDir?>assets/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="<?=adminRootDir?>assets/css/demo.css" />
+    <link rel="stylesheet" href="<?=adminUrl?>assets/vendor/css/rtl/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="<?=adminUrl?>assets/vendor/css/rtl/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="<?=adminUrl?>assets/css/demo.css" />
 
     <!-- Vendors CSS -->
-    <link rel="stylesheet" href="<?=adminRootDir?>assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
-    <link rel="stylesheet" href="<?=adminRootDir?>assets/vendor/libs/typeahead-js/typeahead.css" />
-    <link rel="stylesheet" href="<?=adminRootDir?>assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css" />
-    <link rel="stylesheet" href="<?=adminRootDir?>assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css" />
-    <link rel="stylesheet" href="<?=adminRootDir?>assets/vendor/libs/apex-charts/apex-charts.css" />
-    <link rel="stylesheet" href="<?=adminRootDir?>assets/vendor/libs/swiper/swiper.css" />
+    <link rel="stylesheet" href="<?=adminUrl?>assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="<?=adminUrl?>assets/vendor/libs/typeahead-js/typeahead.css" />
+    <link rel="stylesheet" href="<?=adminUrl?>assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css" />
+    <link rel="stylesheet" href="<?=adminUrl?>assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css" />
+    <link rel="stylesheet" href="<?=adminUrl?>assets/vendor/libs/apex-charts/apex-charts.css" />
+    <link rel="stylesheet" href="<?=adminUrl?>assets/vendor/libs/swiper/swiper.css" />
 
     <!-- Page CSS -->
-    <link rel="stylesheet" href="<?=adminRootDir?>assets/vendor/css/pages/cards-statistics.css" />
-    <link rel="stylesheet" href="<?=adminRootDir?>assets/vendor/css/pages/cards-analytics.css" />
+    <link rel="stylesheet" href="<?=adminUrl?>assets/vendor/css/pages/cards-statistics.css" />
+    <link rel="stylesheet" href="<?=adminUrl?>assets/vendor/css/pages/cards-analytics.css" />
 
     <!-- Helpers -->
-    <script src="<?=adminRootDir?>assets/vendor/js/helpers.js"></script>
+    <script src="<?=adminUrl?>assets/vendor/js/helpers.js"></script>
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Template customizer: To hide customizer set displayCustomizer value false in config.js.  -->
-    <script src="<?=adminRootDir?>assets/vendor/js/template-customizer.js"></script>
+    <script src="<?=adminUrl?>assets/vendor/js/template-customizer.js"></script>
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="<?=adminRootDir?>assets/js/config.js"></script>
+    <script src="<?=adminUrl?>assets/js/config.js"></script>
 </head>
