8e5fbc4c4c686d613c246a14d4783b721b8bd5a7
xmartina <martinsprince126@gmail.com>
2024-01-13 21:00:14 +0100

    i

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,7 @@
 <?php
 const adminRootDir = '/home/multistream6/domains/caketoolnftmarketplace.com/public_html/admin/';
 const adminUrl = 'https://caketoolnftmarketplace.com/admin/';
+$pageName = 'Dashboard';
 include_once (adminRootDir.'includes/adminCore.php');
 include_once (adminRootDir.'includes/generalAdminSettings.php');
 include_once (adminRootDir.'partials/header/main.php');
--- admin/partials/header/parts/page_meta.php
+++ admin/partials/header/parts/page_meta.php
@@ -13,7 +13,7 @@
         name="viewport"
         content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-    <title>Dashboard - CRM | Materialize - Material Design HTML Admin Template</title>
+    <title><?=pageName?> - <?=adminTitle?></title>
 
     <meta name="description" content="" />
 
