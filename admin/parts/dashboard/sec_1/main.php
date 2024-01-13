ac7d9c9a83331d41f87218d5e1e675d5f8e1c3fb
xmartina <martinsprince126@gmail.com>
2024-01-13 21:43:51 +0100

    i

--- admin/parts/dashboard/sec_1/main.php
+++ admin/parts/dashboard/sec_1/main.php
@@ -1,2 +1,2 @@
-<?php include_once (adminRootDir.'admin/parts/dashboard/sec_1/wallet_connect_request.php'); ?>
-<?php include_once (adminRootDir.'admin/parts/dashboard/sec_1/list_users.php'); ?>
+<?php include_once (adminRootDir.'parts/dashboard/sec_1/wallet_connect_request.php'); ?>
+<?php include_once (adminRootDir.'parts/dashboard/sec_1/list_users.php'); ?>
