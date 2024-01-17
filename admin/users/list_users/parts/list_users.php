551bd29199e4b03a58f4d2e9d90780ec7b411f89
xmartina <martinsprince126@gmail.com>
2024-01-17 09:01:34 +0100

    i

--- admin/users/list_users/parts/list_users.php
+++ admin/users/list_users/parts/list_users.php
@@ -5,34 +5,41 @@ while ($data = $result->fetch_assoc()) {
         <td class="control dtr-hidden" tabindex="0" style="display: none;"></td>
         <td class="  dt-checkboxes-cell"><input type="checkbox" class="dt-checkboxes form-check-input">
         </td>
-    <tr class="odd">
-        <td>
+        <td class="sorting_1">
             <div class="d-flex justify-content-start align-items-center user-name">
                 <div class="avatar-wrapper">
-                    <div class="avatar avatar-sm me-2">
-                        <?php
-                        if ($data['user_img'] != '') {
-                            ?>
-                            <img onclick="location.href='<?=adminUrl?>users/edit_user?<?= $data['id'] ?>'"  style="object-fit: contain" src="/assets/images/author/<?=$data['user_img']?>" alt="avatar" class="rounded-circle" />
-                            <?php
-                        } else {
-                            ?>
-                            <img onclick="location.href='<?=adminUrl?>users/edit_user?<?= $data['id'] ?>'" src="<?=adminUrl?>assets/img/avatars/1.png" alt="Avatar" class="rounded-circle">
-                            <?php
-                        }
-                        ?>
+                    <div class="avatar avatar-sm me-3">
+                        <img src="<?= adminUrl ?>assets/img/avatars/4.png" alt="Avatar" class="rounded-circle">
                     </div>
                 </div>
-                <div class="d-flex flex-column ps-1">
-                    <span class="name text-truncate h6 mb-0 mt-1"><?= $data['first_name']. ' '.$data['last_name'] ?></span>
-                    <small class="user_name text-truncate mb-1">@<?= $data['user_name'] ?></small>
+                <div class="d-flex flex-column"><a href="app-user-view-account.html"
+                                                   class="text-truncate"><span
+                            class="fw-medium text-heading">Jonah
+                                            Wharlton</span></a><small>jwharltona@oakley.com</small>
                 </div>
-                <?php if ($data['role'] == 1){ $user_role = 'Admin'; }elseif ($data['role'] == 2){ $user_role = 'User'; } ?>
             </div>
         </td>
-        <td><?= $data['email'] ?></td>
-        <td><span class="d-flex align-items-center gap-2"><i
-                    class="mdi mdi-laptop text-danger"></i><?= $user_role ?></span></td>
-        <td><a href="<?=adminUrl?>users/edit_user?<?= $data['id'] ?>"><span class="badge rounded-pill bg-label-warning">Edit User</span></a></td>
+        <td><span class="text-truncate d-flex align-items-center"><i
+                    class="mdi mdi-account-outline mdi-20px text-primary me-2"></i>Subscriber</span>
+        </td>
+        <td><span class="text-heading">Team</span></td>
+        <td class="" style="">Manual - Paypal</td>
+        <td class="" style=""><span class="badge rounded-pill bg-label-secondary"
+                                    text-capitalized="">Inactive</span>
+        </td>
+        <td class="" style="">
+            <div class="d-inline-block text-nowrap">
+                <button
+                    class="btn btn-sm btn-icon btn-text-secondary rounded-pill btn-icon dropdown-toggle hide-arrow"
+                    data-bs-toggle="dropdown"><i class="mdi mdi-dots-vertical mdi-20px"></i></button>
+                <div class="dropdown-menu dropdown-menu-end m-0"><a href="app-user-view-account.html"
+                                                                    class="dropdown-item"><i
+                            class="mdi mdi-eye-outline me-2"></i><span>View</span></a><a
+                        href="javascript:;" class="dropdown-item"><i
+                            class="mdi mdi-pencil-outline me-2"></i><span>Edit</span></a><a
+                        href="javascript:;" class="dropdown-item delete-record"><i
+                            class="mdi mdi-delete-outline me-2"></i><span>Delete</span></a></div>
+            </div>
+        </td>
     </tr>
-<?php }?>
+<?php } ?>
