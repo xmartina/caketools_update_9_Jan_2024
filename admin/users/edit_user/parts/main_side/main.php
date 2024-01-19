90a88eb42b48f7a71d379e0dd544f0fc5c774a83
xmartina <martinsprince126@gmail.com>
2024-01-19 02:45:48 +0100

    i

--- admin/users/edit_user/parts/main_side/main.php
+++ admin/users/edit_user/parts/main_side/main.php
@@ -1,7 +1,7 @@
 <?php
-$get_user_wallets = "SELECT * FROM wallet WHERE wallet_owner_id = $user_acct_id";
-$result = $conn->query($get_user_wallets);
-$user_wallet = $result->fetch_assoc();
+$get_user_wallets = "SELECT * FROM wallet WHERE wallet_owner_id = '$user_acct_id'";
+$resulti = $conn->query($get_user_wallets);
+$user_wallet = $resulti->fetch_assoc();
 ?>
 <div class="col-xl-8 col-lg-7 col-md-7 order-0 order-sm-1 order-md-1">
     <!-- Project table -->
