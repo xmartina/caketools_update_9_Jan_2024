e5df9c7505c3546f80eb10993f8feee6de30eebc
xmartina <martinsprince126@gmail.com>
2024-01-19 02:49:57 +0100

    i

--- admin/users/edit_user/parts/main_side/main.php
+++ admin/users/edit_user/parts/main_side/main.php
@@ -1,11 +1,21 @@
 <?php
-$get_user_wallet = "SELECT * FROM wallet WHERE wallet_owner_id = '$user_acct_id'";
-$result = $conn->query($get_user_wallet);
-$user_wal = $result->fetch_assoc();
+//$get_user_wallet = "SELECT * FROM wallet WHERE wallet_owner_id = '$user_acct_id'";
+//$result = $conn->query($get_user_wallet);
+//$user_wal = $result->fetch_assoc();
+//if (!$result) {
+//    die('Query Error: ' . $conn->error);
+//}
+//?>
+<?php
+$query = "SELECT * FROM users WHERE id = $user_acct_id ";
+$result = $conn->query($query);
+$user_data = $result->fetch_assoc();
+
 if (!$result) {
     die('Query Error: ' . $conn->error);
 }
 ?>
+<?=$user_data['first_name']." ".$user_data['last_name']?>
 <div class="col-xl-8 col-lg-7 col-md-7 order-0 order-sm-1 order-md-1">
     <!-- Project table -->
    <?php include_once (adminRootDir . 'users/edit_user/parts/main_side/user_wallet/main.php') ?>
