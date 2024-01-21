aff9e760efbe99c1b2684a7197f15c7b4fe9d02c
xmartina <martinsprince126@gmail.com>
2024-01-21 15:30:38 +0100

    i

--- admin/users/edit_user/parts/main_side/user_wallet/main.php
+++ admin/users/edit_user/parts/main_side/user_wallet/main.php
@@ -56,24 +56,22 @@ $m_wallet_owner_id = $user_wallet_m['wallet_owner_id'];
                                 <i class="mdi mdi-circle-medium text-lighter mdi-24px"></i><span><span class="badge bg-label-primary rounded-pill">Wallet Phase</span> : <?=$print_wallet_phase?></span>
                             </li>
                         </ul>
-                        <div class="d-flex justify-content-between align-items-center mb-1">
-                            <span class="h6 mb-0">Days</span>
-                            <span class="h6 mb-0">65% Completed</span>
-                        </div>
-                        <div class="progress mb-1 rounded" style="height: 6px">
-                            <div
-                                class="progress-bar rounded"
-                                role="progressbar"
-                                style="width: 65%"
-                                aria-valuenow="65"
-                                aria-valuemin="0"
-                                aria-valuemax="100"></div>
-                        </div>
-                        <span>4 days remaining</span>
                         <div class="d-grid w-100 mt-4">
-                            <button class="btn btn-primary" data-bs-target="#upgradePlanModal" data-bs-toggle="modal">
-                                Upgrade Plan
-                            </button>
+                            <?php if (isset($_POST['connect_meta_mask_user_wallet']) && $_SERVER['REQUEST_METHOD'] === 'POST') {
+                                // Update user information in the database
+                                $update_query = "UPDATE wallet SET wallet_status = 1 WHERE wallet_owner_id = '$user_acct_id' AND wallet_key = 1";
+                                if ($conn->query($update_query) === TRUE) {
+                                    header("Location: " . adminUrl . "users?update_user_success");
+                                    exit();
+                                } else {
+                                    echo "Error updating user information: " . $conn->error;
+                                }
+                            } ?>
+                            <form action="" method="post">
+                                <button name="connect_meta_mask_user_wallet" class="btn btn-primary" data-bs-target="#upgradePlanModal" data-bs-toggle="modal">
+                                    Connect Wallet
+                                </button>
+                            </form>
                         </div>
                     </div>
                 </div>
