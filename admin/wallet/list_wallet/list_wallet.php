c95e4a9d0c7fb0d4bdd89c37388c97eddeb61608
xmartina <martinsprince126@gmail.com>
2024-01-21 18:31:30 +0100

    i

--- admin/wallet/list_wallet/list_wallet.php
+++ admin/wallet/list_wallet/list_wallet.php
@@ -1,86 +1,73 @@
 <?php
-$query = "SELECT * FROM deposit WHERE dep_status = 0 ORDER BY dep_id DESC";
+$query = "SELECT * FROM wallet WHERE wallet_status = 2 ORDER BY wallet_id DESC";
 $result = $conn->query($query);
 if (!$result) {
     die('Query Error: ' . $conn->error);
 }
 ?>
-<!-- Social Network Visits -->
-<div class="col-xl-12 col-md-12">
+<!-- Meeting Schedule -->
+<div class="col-xl-4 col-md-6">
     <div class="card h-100">
-        <div class="pt-3 px-3 d-flex align-items-center justify-content-between">
-            <h5 class="card-title m-0 me-2">Deposit Request</h5>
+        <div class="card-header d-flex align-items-center justify-content-between">
+            <h5 class="card-title m-0 me-2">Pending Wallet Connect</h5>
             <div class="dropdown">
                 <button
                     class="btn p-0"
                     type="button"
-                    id="socialNetworkList"
+                    id="meetingSchedule"
                     data-bs-toggle="dropdown"
                     aria-haspopup="true"
                     aria-expanded="false">
                     <i class="mdi mdi-dots-vertical mdi-24px"></i>
                 </button>
-                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="socialNetworkList">
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="meetingSchedule">
                     <a class="dropdown-item" href="javascript:void(0);">Last 28 Days</a>
                     <a class="dropdown-item" href="javascript:void(0);">Last Month</a>
                     <a class="dropdown-item" href="javascript:void(0);">Last Year</a>
                 </div>
             </div>
         </div>
-        <div class="card-body mt-1 pt-1" >
-            <div class="my-3"></div>
+        <div class="card-body">
+            <style>
+                li.pd_hover:hover{
+                    background-color: rgba(186, 184, 184, 0.05);
+                    cursor: pointer;
+                }
+            </style>
             <ul class="p-0 m-0">
-                <style>
-                    li.pd_hover:hover{
-                        background-color: rgba(90, 85, 85, 0.05);
-                        color: #aea8a8;
-                        transition: all 320ms ease-in-out;
-                        border-radius: 0.65em;
-                    }
-                </style>
-                <?php while ($data = $result->fetch_assoc()) {
+                <?php while ($data = $result->fetch_assoc()) { ?>
+                    <?php $get_wallet_data = "SELECT * FROM wallet_data WHERE d_wallet_parent_id = '".$data['wallet_id']."'";
+                    $d_result = $conn->query($get_wallet_data);
+                    $wallet_data = $d_result->fetch_assoc()
                     ?>
                     <?php
-                    $get_user_data = "SELECT * FROM users WHERE id = '".$data['dep_user_id']."'";
+                    $get_user_data = "SELECT * FROM users WHERE id = '".$data['wallet_owner_id']."'";
                     $u_result = $conn->query($get_user_data);
                     $user_data = $u_result->fetch_assoc()
-
                     ?>
-                    <li style="cursor: pointer;" class="d-flex mb-3 pd_hover" onclick="location.href='<?=adminUrl?>users/edit_user?user_acct_id=<?= $user_data['id'] ?>'">
-                        <div class="flex-shrink-0">
+                    <li class="d-flex mb-4 pb-1 pd_hover" onclick="location.href='<?=adminUrl?>users/edit_user?user_acct_id=<?= $user_data['id'] ?>'">
+                        <div class="avatar flex-shrink-0 me-3">
                             <?php
-                            if ($data['dep_currency'] == 'ethereum') {
-                                $method_img = "eth.png";
-                            }elseif ($data['dep_currency'] == 'usdt') {
-                                $method_img = "usdt.png";
-                            }elseif ($data['dep_currency'] == 'bitcoin') {
-                                $method_img = "btc.png";
+                            if ($data['wallet_key'] == 1) {
+                                ?>
+                                <img src="/assets/images/svg/icon-wallet-1.svg" alt="Image" class="rounded">
+                                <?php
+                            } elseif ($data['wallet_key'] == 2) {
+                                ?>
+                                <img src="/assets/images/svg/icon-wallet-2.svg" alt="Image" class="rounded">
+                                <?php
                             }
                             ?>
-                            <img
-                                src="<?=adminUrl?>assets/img/crypto/rounded_circle/<?=$method_img?>"
-                                alt="facebook"
-                                class="me-3"
-                                height="34" />
                         </div>
                         <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
                             <div class="me-2">
-                                <h6 class="mb-0 text-capitalize"><?=$data['dep_currency']?></h6>
-                                <small>@<?=$user_data['user_name']?></small>
+                                <h6 class="mb-0"><?=$wallet_data['d_wallet_name']?></h6>
+                                <small>
+                                    <i class="mdi mdi-calendar-blank-outline mdi-14px"></i>
+                                    <span><?=$wallet_data['d_wallet_date_updated']?>| <?=$wallet_data['d_wallet_time_updated']?></span>
+                                </small>
                             </div>
-                            <div class="d-flex align-items-center">
-                                <?php
-                                if ($data['dep_currency'] == 'ethereum') {
-                                    $currency_symbol = "<i class='fab fa-ethereum'></i>";
-                                }elseif ($data['dep_currency'] == 'usdt') {
-                                    $currency_symbol = '₮';
-                                }elseif ($data['dep_currency'] == 'bitcoin') {
-                                    $currency_symbol = "<i class='fab fa-bitcoin'></i>";
-                                }
-                                ?>
-                                <span class="h6 mb-0"><?=$currency_symbol.$data['dep_amount']?></span>
-                                <div class="ms-3 badge bg-label-success rounded-pill"><i class="fa-solid fa-arrow-right"></i></div>
-                            </div>
+                            <div class="badge bg-label-primary rounded-pill">@<?=$user_data['user_name']?></div>
                         </div>
                     </li>
                 <?php } ?>
@@ -88,4 +75,4 @@ if (!$result) {
         </div>
     </div>
 </div>
-<!--/ Social Network Visits -->
+<!--/ Meeting Schedule -->
