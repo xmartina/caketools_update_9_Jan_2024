b523d15d9cd720574137b609f3f99a525ed70268
xmartina <martinsprince126@gmail.com>
2024-01-27 00:53:51 +0100

    i

--- add_nft/left/parts/sec_1.php
+++ add_nft/left/parts/sec_1.php
@@ -3,7 +3,7 @@
     <div class="add-nft-inner">
         <h6 class="title">Choose Category</h6>
         <p class="sub">Connect With One Of Our Available Wallet Providers Or Create A New One.</p>
-        <select class="blockchain-button p-3" style="border-radius: 3px; background-color: #191820; color: #d7d7d7; width:250px; height: 35px; border: .7px solid #d7d7d7; text-align:center; font-weight: bold;">
+        <select class="blockchain-button p-1" style="border-radius: 3px; background-color: #191820; color: #d7d7d7; width:250px; height: 39px; border: .7px solid #d7d7d7; text-align:center; font-weight: bold;">
             <?php
             while ($g_c_row = $g_category_result->fetch_assoc()) {
                 ?>
