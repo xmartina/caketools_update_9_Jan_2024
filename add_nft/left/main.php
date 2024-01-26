76422165ff25d88a594193215b11b612022d2865
xmartina <martinsprince126@gmail.com>
2024-01-26 23:25:09 +0100

    i

---
+++ add_nft/left/main.php
@@ -0,0 +1,4 @@
+<?php
+include_once (rootDir.'add_nft/left/parts/sec_1.php');
+include_once (rootDir.'add_nft/left/parts/sec_2.php');
+include_once (rootDir.'add_nft/left/parts/sec_3.php');
---
+++ add_nft/right/main.php
@@ -0,0 +1,44 @@
+<div class="col-xl-3 col-lg-4 col-md-6">
+    <h5 class="title-preview">Item Preview</h5>
+    <div class="sc-product style1">
+        <div class="top">
+            <a href="#" class="tag">Sweet Baby #1</a>
+            <div class="wish-list">
+                <a href="#" class="heart-icon"></a>
+            </div>
+        </div>
+        <div class="features">
+            <div class="product-media">
+                <img src="/assets/images/product/product4.jpg" alt="images">
+            </div>
+            <div class="featured-countdown">
+                <span class="js-countdown" data-timer="55555" data-labels=" ,  h , m , s "></span>
+            </div>
+            <div class="rain-drop1"><img src="/assets/images/icon/rain1.svg" alt="images"></div>
+            <div class="rain-drop2"><img src="/assets/images/icon/rain2.svg" alt="images"></div>
+        </div>
+        <div class="bottom">
+            <div class="details-product">
+                <div class="author">
+                    <div class="avatar">
+                        <img src="/assets/images/author/author1.png" alt="images">
+                    </div>
+                    <div class="content">
+                        <div class="position">Creator</div>
+                        <div class="name"> <a href="#">Carly Webster</a></div>
+                    </div>
+                </div>
+                <div class="current-bid">
+                    <div class="subtitle">Current bid</div>
+                    <div class="price">
+                        <span class="cash">5 ETH</span><span class="icon"><img src="/assets/images/icon/ethe.svg" alt="images"></span>
+                    </div>
+                </div>
+            </div>
+            <div class="product-button">
+                <a href="#" data-toggle="modal" data-target="#popup_bid" class="tf-button"> <span class="icon-btn-product"></span> Place Bid</a>
+            </div>
+
+        </div>
+    </div>
+</div>
