91bcd8af57cfd40dc22e8caa9d026c9d74b83013
xmartina <martinsprince126@gmail.com>
2024-01-27 01:14:13 +0100

    i

--- add_nft/left/parts/sec_2.php
+++ add_nft/left/parts/sec_2.php
@@ -1,104 +1,32 @@
-<fieldset class="propertise">
-    <label class="mb8">Add properties</label>
-    <ul class="propertise-list">
-        <li><a href="#">Art<i class="fal fa-times"></i></a></li>
-        <li><a href="#">Body type<i class="fal fa-times"></i></a></li>
-        <li><a href="#">Face color<i class="fal fa-times"></i></a></li>
-    </ul>
-</fieldset>
-
+<!--NFT Properties-->
+<div class="infor">
+    <p>NFT Properties</p>
+    <h6>Tell us about your NFT</h6>
+</div>
 <div class="set-item">
     <fieldset>
-        <label class="mb8">Set item price or starting bid</label>
-        <input type="text"  placeholder="E.G. 0,01 Eth">
+        <label class="mb8">Background Color</label>
+        <input name="p_background" type="text"  placeholder="E.G. Blue">
     </fieldset>
     <fieldset>
-        <label class="mb8">Select royalties amount, %</label>
-        <input type="text"  placeholder="E.G. 0,01 Eth">
+        <label class="mb8">Mouth Grade</label>
+        <input name="p_mouth_grade" type="text"  placeholder="E.G. fresh">
     </fieldset>
 </div>
 
-<h6 class="title ">Choose Collection</h6>
-<p class="sub">They All Serve The Same Purpose.</p>
-
-<ul class="create-collection">
-    <li class="active">
-        <div class="create-item">
-            <div class="img"><i class="fal fa-plus"></i></div>
-            <div class="content">
-                <h6>Create new collection</h6>
-                <p>Type to create</p>
-            </div>
-        </div>
-    </li>
-    <li>
-        <div class="create-item">
-            <div class="img"><img src="/assets/images/collection/add-collection.jpg" alt="Image"></div>
-            <div class="content">
-                <h6>Battle for Digital</h6>
-                <p>56 items</p>
-            </div>
-        </div>
-    </li>
-</ul>
-
-<h6 class="title mb0">Choose collection</h6>
-<p class="sub mb20">They all serve the same purpose.</p>
-
-<ul class="collection-list">
-    <li>
-        <div class="list">
-            <div class="infor">
-                <p>Product updates</p>
-                <h6>Receive messages from our platform</h6>
-            </div>
-
-            <div  class="button-toggle">
-                <input type="checkbox" id="switch" checked >
-                <label for="switch"></label>
-            </div>
-        </div>
-    </li>
-    <li>
-        <div class="list">
-            <div class="infor">
-                <p>Reminders</p>
-                <h6>Receive booking reminders, pricing notices</h6>
-            </div>
-            <div class="button-toggle mt0">
-                <input  type="checkbox" id="switch1">
-                <label for="switch1"></label>
-            </div>
-        </div>
-    </li>
-    <li>
-        <div class="list">
-            <div class="infor">
-                <p>Promotions and tips</p>
-                <h6>Receive coupons, promotions, surveys</h6>
-            </div>
-            <div class="button-toggle">
-                <input  type="checkbox" id="switch2" checked>
-                <label for="switch2"></label>
-            </div>
-        </div>
-    </li>
-    <li>
-        <div class="list">
-            <div class="infor">
-                <p>Account support</p>
-                <h6>Receive messages about your account, your trips, legal alerts</h6>
-            </div>
-            <div class="button-toggle">
-                <input  type="checkbox" id="switch3">
-                <label for="switch3"></label>
-            </div>
-        </div>
-    </li>
-</ul>
+<div class="set-item">
+    <fieldset>
+        <label class="mb8">Head</label>
+        <input name="p_head" type="text"  placeholder="E.G. Bowlcut">
+    </fieldset>
+    <fieldset>
+        <label class="mb8">Body Color</label>
+        <input name="p_body" type="text"  placeholder="E.G. Dark Red">
+    </fieldset>
+</div>
 
 <div class="bottom-button">
-    <a href="#" class="tf-button active">Publish</a>
+    <button type="submit" name="create_new_nft" class="tf-button active">Publish</button>
     <a href="#" class="tf-button">Discard all</a>
 </div>
 </div>
--- add_nft/left/removed_blocks.php
+++ add_nft/left/removed_blocks.php
@@ -151,3 +151,95 @@
         </div>
     </div>
 </div>
+
+
+<!--NFT Properties page-->
+<h6 class="title ">Choose Collection</h6>
+<p class="sub">They All Serve The Same Purpose.</p>
+
+<ul class="create-collection">
+    <li class="active">
+        <div class="create-item">
+            <div class="img"><i class="fal fa-plus"></i></div>
+            <div class="content">
+                <h6>Create new collection</h6>
+                <p>Type to create</p>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="create-item">
+            <div class="img"><img src="/assets/images/collection/add-collection.jpg" alt="Image"></div>
+            <div class="content">
+                <h6>Battle for Digital</h6>
+                <p>56 items</p>
+            </div>
+        </div>
+    </li>
+</ul>
+
+<h6 class="title mb0">Choose collection</h6>
+<p class="sub mb20">They all serve the same purpose.</p>
+
+<ul class="collection-list">
+    <li>
+        <div class="list">
+            <div class="infor">
+                <p>Product updates</p>
+                <h6>Receive messages from our platform</h6>
+            </div>
+
+            <div  class="button-toggle">
+                <input type="checkbox" id="switch" checked >
+                <label for="switch"></label>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="list">
+            <div class="infor">
+                <p>Reminders</p>
+                <h6>Receive booking reminders, pricing notices</h6>
+            </div>
+            <div class="button-toggle mt0">
+                <input  type="checkbox" id="switch1">
+                <label for="switch1"></label>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="list">
+            <div class="infor">
+                <p>Promotions and tips</p>
+                <h6>Receive coupons, promotions, surveys</h6>
+            </div>
+            <div class="button-toggle">
+                <input  type="checkbox" id="switch2" checked>
+                <label for="switch2"></label>
+            </div>
+        </div>
+    </li>
+    <li>
+        <div class="list">
+            <div class="infor">
+                <p>Account support</p>
+                <h6>Receive messages about your account, your trips, legal alerts</h6>
+            </div>
+            <div class="button-toggle">
+                <input  type="checkbox" id="switch3">
+                <label for="switch3"></label>
+            </div>
+        </div>
+    </li>
+</ul>
+
+
+
+<fieldset class="propertise">
+    <label class="mb8">Add properties</label>
+    <ul class="propertise-list">
+        <li><a href="#">Art<i class="fal fa-times"></i></a></li>
+        <li><a href="#">Body type<i class="fal fa-times"></i></a></li>
+        <li><a href="#">Face color<i class="fal fa-times"></i></a></li>
+    </ul>
+</fieldset>
