6bdd146357f69526e33a8dc12d6184502f08ab25
xmartina <martinsprince126@gmail.com>
2024-01-28 13:12:43 +0100

    i

---
+++ user_deposit/index.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+ob_start();
+if (!isset($_SESSION['user_id'])) { ?>
+    <script>
+        window.location.href = '/auth/login';
+    </script>
+    <?php
+    exit();
+}
+$user_id = $_SESSION['user_id'];
+const pageName = 'Create New NFT';
+const rootDir = '/home/multistream6/domains/caketoolnftmarketplace.com/public_html/';
+include_once (rootDir.'includes/generalConfig.php');
+include_once (rootDir.'includes/core.php');
+include_once (rootDir.'partials/front/header/main.php');
+
+include_once (rootDir.'user_deposit/parts/main.php');
+?>
+
+<!-- title page -->
+<div class="py-5"></div>
+<section class="tf-page-title">
+    <div class="tf-container">
+        <div class="row">
+            <div class="col-md-12">
+
+                <!--                <ul class="breadcrumbs">-->
+                <!--                    <li><a href="index.html">Home</a></li>-->
+                <!--                    <li>Create</li>-->
+                <!--                </ul>-->
+
+                <h4 class="page-title-heading">Add New NFT</h4>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+include_once(rootDir . 'partials/front/footer/main.php'); ?>
+?>
