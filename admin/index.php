c871222d51f352577f1a7eecd0e442f142c200c1
xmartina <martinsprince126@gmail.com>
2024-01-30 04:17:30 +0100

    i

--- admin/index.php
+++ admin/index.php
@@ -1,12 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['admin_id'])) { ?>
-    <script>
-        window.location.href = '/admin_auth';
-    </script>
-    <?php
-    exit();
-}
+$admin_id = $_SESSION['admin_id'];
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 const adminRootDir = '/home/multistream6/domains/caketoolnftmarketplace.com/public_html/admin/';
