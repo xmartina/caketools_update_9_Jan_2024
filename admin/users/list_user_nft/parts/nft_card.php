1349cfd0b16031349761577d9874ac8a5c303359
xmartina <martinsprince126@gmail.com>
2024-01-30 01:39:03 +0100

    i

--- admin/users/list_user_nft/parts/nft_card.php
+++ admin/users/list_user_nft/parts/nft_card.php
@@ -8,39 +8,37 @@
         $get_nft_count = "SELECT COUNT(*) as total_rows FROM nft_parent WHERE id = $nft_data[id]";
         $result = $conn->query($get_nft_count);
         // Check if the query was successful
-        if ($result) {
-        $row = $result->fetch_assoc();
-        $user_total_nft = $row['total_rows'];
-      ?>
-    <?php } elseif($nft_data['nft_img'] != '') {
-    ?>
-    <div class="col-sm-6 col-lg-4 mb-4">
-        <div class="card">
-            <img class="card-img-top" src="<?=siteUrl?>assets/images/nfts/<?=$nft_data['nft_img']?>" alt="<?=$nft_data['name']?>" style="height: 300px; object-fit: cover" />
-            <div class="card-body">
-                <h5 class="card-title"><?=$nft_data['name']?></h5>
-                <p class="card-text">
-                    <?=$nft_data['description']?>
-                </p>
-            </div>
-        </div>
+        if ($result) { ?>
+            <div class="col-sm-6 col-lg-4 mb-4">
+                <div class="card">
+                    <img class="card-img-top" src="<?=siteUrl?>assets/images/nfts/<?=$nft_data['nft_img']?>" alt="<?=$nft_data['name']?>" style="height: 300px; object-fit: cover" />
+                    <div class="card-body">
+                        <h5 class="card-title"><?=$nft_data['name']?></h5>
+                        <p class="card-text">
+                            <?=$nft_data['description']?>
+                        </p>
+                    </div>
+                </div>
 
 
-        <div class="row">
-            <div class="col-sm-12 col-lg-12 mb-12 py-3">
-                <div class="card p-3">
-                    <figure class="p-3 mb-0">
-                        <blockquote class="blockquote">
-                            <p>A well-known quote, contained in a blockquote element.</p>
-                        </blockquote>
-                        <figcaption class="blockquote-footer mb-0 text-muted">
-                            Someone famous in <cite title="Source Title">Source Title</cite>
-                        </figcaption>
-                    </figure>
+                <div class="row">
+                    <div class="col-sm-12 col-lg-12 mb-12 py-3">
+                        <div class="card p-3">
+                            <figure class="p-3 mb-0">
+                                <blockquote class="blockquote">
+                                    <p>A well-known quote, contained in a blockquote element.</p>
+                                </blockquote>
+                                <figcaption class="blockquote-footer mb-0 text-muted">
+                                    Someone famous in <cite title="Source Title">Source Title</cite>
+                                </figcaption>
+                            </figure>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
+    <?php } elseif($nft_data['nft_img'] != '') {
+    ?>
+     No NFT
     <?php } ?>
     <?php } ?>
 </div>
