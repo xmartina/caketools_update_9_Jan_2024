0fc78a171c7b34c0e7a997f21b0cb184732ee491
xmartina <martinsprince126@gmail.com>
2024-01-30 01:01:18 +0100

    i

--- admin/users/list_user_nft/parts/nft_card.php
+++ admin/users/list_user_nft/parts/nft_card.php
@@ -9,10 +9,9 @@
         <div class="card">
             <img class="card-img-top" src="<?=siteUrl?>assets/images/nfts/<?=$nft_data['nft_img']?>" alt="<?=$nft_data['name']?>" />
             <div class="card-body">
-                <h5 class="card-title">Card title that wraps to a new line</h5>
+                <h5 class="card-title"><?=$nft_data['name']?></h5>
                 <p class="card-text">
-                    This is a longer card with supporting text below as a natural lead-in to additional content.
-                    This content is a little bit longer.
+                    <?=$nft_data['description']?>
                 </p>
             </div>
         </div>
