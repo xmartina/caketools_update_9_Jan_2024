98575338f5dbd25ce035844c4274ec4c490d62ed
xmartina <martinsprince126@gmail.com>
2024-01-30 04:27:35 +0100

    i

--- admin/includes/adminCore.php
+++ admin/includes/adminCore.php
@@ -1,3 +1,14 @@
 <?php
+session_start();
+ob_start();
+if (!isset($_SESSION['admin_id'])) { ?>
+    <script>
+        window.location.href = '/admin_auth';
+    </script>
+<?php }
+$admin_id = $_SESSION['admin_id'];
+
 const rootDir = '/home/multistream6/domains/caketoolnftmarketplace.com/public_html/';
 include_once (rootDir.'includes/core.php');
+
+?>
--- admin/index.php
+++ admin/index.php
@@ -1,13 +1,4 @@
 <?php
-session_start();
-ob_start();
-if (!isset($_SESSION['admin_id'])) { ?>
-    <script>
-        window.location.href = '/admin_auth';
-    </script>
-<?php } ?>
-<?php
-$admin_id = $_SESSION['admin_id'];
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 const adminRootDir = '/home/multistream6/domains/caketoolnftmarketplace.com/public_html/admin/';
