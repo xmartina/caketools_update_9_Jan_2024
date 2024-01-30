2a0c31c08ddb8e47c507979e6aeeab20159bdaf8
xmartina <martinsprince126@gmail.com>
2024-01-30 01:18:42 +0100

    i

--- partials/front/footer/main.php
+++ partials/front/footer/main.php
@@ -127,6 +127,15 @@
 <script src="/assets/js/main.js"></script>
 <script src="/assets/js/front_footer_js.js"></script>
 
+<!--whatsapp btn-->
+<script async src='https://d2mpatx37cqexb.cloudfront.net/delightchat-whatsapp-widget/embeds/embed.min.js'></script>
+<script>
+    var wa_btnSetting = {"btnColor":"#fac400","ctaText":"WhatsApp Us","cornerRadius":40,"marginBottom":20,"marginLeft":20,"marginRight":20,"btnPosition":"right","whatsAppNumber":"971581761096","welcomeMessage":"Hello","zIndex":999999,"btnColorScheme":"light"};
+    window.onload = () => {
+        _waEmbed(wa_btnSetting);
+    };
+</script>
+
 </body>
 
 </html>
